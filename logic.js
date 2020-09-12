b7ca35aa2e6f8a96cadffbf2d33170b0a45b3c69
harshita nahta <harshitajainnahta@gmail.com>
2020-09-12 14:44:43 +0530

    first commit

---
+++ logic.js
@@ -0,0 +1,35 @@
+$(document).ready(()=>{
+  $('.div1')
+  		.css('font-size', '40px')
+      .css('text-align', 'center')
+      .text('Hii!')
+      .addClass('centered')
+      .fadeOut(2000)
+      .fadeIn(2000)
+      .fadeOut(2500,()=>{
+              $('.div1')
+                .css('font-size', '40px')
+                .text('I am Harshita!')
+                .fadeIn(1500,()=>{
+                        $('.div2')
+                          .css('font-size', '40px')
+                          .text('A Girl who <code>')
+                          .addClass('typeWriterEffect')
+                          .delay(3500)
+                          .fadeOut(1500)
+                      })
+                      .delay(5000)
+                      .fadeOut(2500,()=>{
+                              $('.div1')
+                                .css('font-size', '40px')
+                                .text('Here is my portfolio')
+                                .fadeIn(1500)
+                                .fadeOut(1500)
+                                .fadeOut(1500)
+                                .fadeOut(3500,()=>{
+                                        $('.computer_img')
+                                          .fadeOut(1500)
+                                                  })
+                                        });
+                    });
+});
